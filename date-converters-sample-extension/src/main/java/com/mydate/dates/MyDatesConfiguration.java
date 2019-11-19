e55b930949ddd0d11180fada44da5091f2108140
beirtipol <beirtipol@gmail.com>
2019-11-19 10:56:40 +0000

    Add some more config files and configure maven for OSSRH deployment

--- date-converters-sample-extension/src/main/java/com/mydate/dates/MyDatesConfiguration.java
+++ date-converters-sample-extension/src/main/java/com/mydate/dates/MyDatesConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import com.beirtipol.dates.Converters;
 
 /**
- * You are required to include {@link DatesConfiguration} in your @ComponentScan in order to ensure correct
+ * You are required to include this class in your @ComponentScan in order to ensure correct
  * initialization
  * 
  * @author beirtipol@gmail.com
